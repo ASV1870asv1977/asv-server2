d618731bb116f4654bb99b1953b43962da9c4594
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-08-09 12:17:18 +0300

    added avatar for user in authapp/pipeline

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode, urlunparse
 
 import requests
 from django.utils import timezone
+from django.conf import settings
 from social_core.exceptions import AuthForbidden
 
 from authapp.models import ShopUserProfile
@@ -13,42 +14,42 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if backend.name != 'vk-oauth2':
         return
 
-    # api_url = f'{settings.VK_API_URL}method/users.get/'
-    #
-    # params = {
-    #     "fields": "bdate,sex,about",
-    #     "access_token": response["access_token"],
-    #     "v": "5.92"
-    # }
-    #
-    # requests.get(api_url, params=params)
+    api_url = urlunparse(('https',
+                          'api.vk.com',
+                          '/method/users.get',
+                          None,
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_max_orig')),
+                                                access_token=response['access_token'],
+                                                v='5.92')),
+                          None
+                          ))
 
-    api_url = f'https://api.vk.com/method/users.get/?fields=bdate,sex,about&access_token={response["access_token"]}&v=5.92'
-
-    response = requests.get(api_url)
-    if response.status_code != 200:
+    resp = requests.get(api_url)
+    if resp.status_code != 200:
         return
 
-    data = response.json()['response'][0]
+    data = resp.json()['response'][0]
+    if data['sex']:
+        user.shopuserprofile.gender = ShopUserProfile.MALE if data['sex'] == 2 else ShopUserProfile.FEMALE
 
-    if 'sex' in data:
-        if data['sex'] == 1:
-            user.shopuserprofile.gender = ShopUserProfile.FEMALE
-        elif data['sex'] == 2:
-            user.shopuserprofile.gender = ShopUserProfile.MALE
+    if 'photo_max_orig' in data:
+        photo_content = requests.get(data['photo_max_orig'])
+        with open(f'{settings.MEDIA_ROOT}/users_avatars/{user.pk}.jpg', 'wb') as photo_file:
+            photo_file.write(photo_content.content)
+            user.avatar = f'users_avatars/{user.pk}.jpg'
 
-    if 'about' in data:
-        user.shopuserprofile.about_me = data['about']
+    if data['about']:
+        user.shopuserprofile.aboutME = data['about']
 
     # Что-то с данным куском кода выдает ошибку
-    # if data['bdate']:
-    #     bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
-    #
-    #     age = timezone.now().date().year - bdate.year
-    #     if age < 18:
-    #         user.delete()
-    #         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
-    #
-    #     user.age = age
+    if data['bdate']:
+        bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+
+        age = timezone.now().date().year - bdate.year
+        if age < 18:
+            user.delete()
+            raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+        user.age = age
 
     user.save()
